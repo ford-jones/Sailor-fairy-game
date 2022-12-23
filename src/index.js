1886e37ba7550924f33986b65bd31db7f312911e
ford-jones <fordnicholasjones@gmail.com>
2022-12-23 23:45:21 +1300

    reconfigure and update dependencies for electron

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import Game from './components/Game'
+/* eslint-disable space-before-function-paren */
+/* eslint-disable quotes */
+/* eslint-disable semi */
 
-window.addEventListener('DOMContentLoaded', () => {
-  let game = new Game('babylonCanvas')
+import Game from "./components/Game";
 
-  game.createScene()
+window.addEventListener("DOMContentLoaded", () => {
+  // const replaceText = (selector, text) => {
+  //   const element = document.getElementById(selector);
+  //   if (element) element.innerText = text;
+  // };
 
-  game.doRender()
-})
+  // for (const dependency of ["chrome", "node", "electron"]) {
+  //   replaceText(`${dependency}-version`, process.versions[dependency]);
+  // }
+  let game = new Game("babylonCanvas");
+
+  game.createScene();
+
+  game.doRender();
+});
