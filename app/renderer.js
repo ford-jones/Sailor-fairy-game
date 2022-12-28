71e5664352aeb556069dd695af1ace96f52d5f03
ford-jones <fordnicholasjones@gmail.com>
2022-12-28 22:39:52 +1300

    push into remote to send through wetransfer on another machine

--- app/renderer.js
+++ app/renderer.js
@@ -66,22 +66,6 @@ class Renderer {
                     ground.checkCollisions = true;
                     ground.material = groundMaterial;
                 });
-                //  SPRITES
-                let flashBook = new Array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
-                flashBook.forEach(() => {
-                    let flashIndex = flashBook[Math.floor(Math.random() * flashBook.length)];
-                    let flashTransfer = new Array;
-                    const spriteManagerFlash = new BABYLON.SpriteManager('flash', `./assets/sprites/sprite${flashIndex}.png`, 30, 450, scene);
-                    for (let z = 0; z < 100; z++) {
-                        let flash = new BABYLON.Sprite('flash', spriteManagerFlash);
-                        flash.position.x = Math.floor(Math.random() * 300 - 150);
-                        flash.position.z = Math.floor(Math.random() * 300 - 150);
-                        flash.position.y = 8;
-                        flash.height = 5;
-                        flash.width = 5;
-                        flashTransfer.push(flash);
-                    }
-                });
             });
         }
         ;
@@ -89,7 +73,7 @@ class Renderer {
         //  TEXT
         setTimeout(() => {
             const header = document.getElementById('header');
-            header.innerHTML = "I love you dotti!";
+            header.innerHTML = "Merry christmas mum!";
         }, 3000);
     }
     /* RENDER LOOP */
--- src/renderer.ts
+++ src/renderer.ts
@@ -79,27 +79,6 @@ export default class Renderer {
              ground.material = groundMaterial
 
           })
-
-          //  SPRITES
-          let flashBook = new Array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
-          flashBook.forEach(() => {
-            let flashIndex = flashBook[Math.floor(Math.random() * flashBook.length)]
-            let flashTransfer = new Array
-
-            const spriteManagerFlash = new BABYLON.SpriteManager('flash', `./assets/sprites/sprite${flashIndex}.png`, 30, 450, scene);
-  
-            for(let z = 0; z < 100; z++) {
-              let flash = new BABYLON.Sprite('flash', spriteManagerFlash);
-              flash.position.x = Math.floor(Math.random() * 300 - 150);
-              flash.position.z = Math.floor(Math.random() * 300 - 150);
-              flash.position.y = 8
-              flash.height = 5
-              flash.width = 5
-              flashTransfer.push(flash)
-            }
-          })
-
-          
           
       } ;
       environment('MazeV1', scene);
@@ -107,7 +86,7 @@ export default class Renderer {
       //  TEXT
       setTimeout(() => {
         const header = document.getElementById('header') as HTMLParagraphElement;
-        header.innerHTML = "I love you dotti!"
+        header.innerHTML = "Merry christmas mum!"
         
       }, 3000);
       
